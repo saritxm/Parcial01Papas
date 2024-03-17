aa9376fda97764586f3db19df5cd1025de1995de
saritxm <saranicollemm@gmail.com>
2024-03-17 18:15:28 -0500

    guardar

--- src/view/PanelConsultar.java
+++ src/view/PanelConsultar.java
@@ -8,12 +8,12 @@ package view;
  *
  * @author Sara
  */
-public class PanelConsultar extends javax.swing.JPanel {
+public class PanelConsultarE extends javax.swing.JPanel {
 
     /**
-     * Creates new form PanelConsultar
+     * Creates new form PanelConsultarE
      */
-    public PanelConsultar() {
+    public PanelConsultarE() {
         initComponents();
     }
 
@@ -26,19 +26,137 @@ public class PanelConsultar extends javax.swing.JPanel {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(this);
-        this.setLayout(layout);
-        layout.setHorizontalGroup(
-            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 682, Short.MAX_VALUE)
-        );
-        layout.setVerticalGroup(
-            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 372, Short.MAX_VALUE)
-        );
+        jComboBox1 = new javax.swing.JComboBox<>();
+        jLabel2 = new javax.swing.JLabel();
+        jLabel3 = new javax.swing.JLabel();
+        jLabel4 = new javax.swing.JLabel();
+        jLabel5 = new javax.swing.JLabel();
+        jLabel1 = new javax.swing.JLabel();
+        jLabel6 = new javax.swing.JLabel();
+        jLabel7 = new javax.swing.JLabel();
+        jLabel8 = new javax.swing.JLabel();
+        jTextField1 = new javax.swing.JTextField();
+        jTextField2 = new javax.swing.JTextField();
+        jTextField3 = new javax.swing.JTextField();
+        jTextField4 = new javax.swing.JTextField();
+        jTextField5 = new javax.swing.JTextField();
+        jTextField6 = new javax.swing.JTextField();
+        jTextField7 = new javax.swing.JTextField();
+        jButton1 = new javax.swing.JButton();
+        jButton2 = new javax.swing.JButton();
+        jButton3 = new javax.swing.JButton();
+        Fondo = new javax.swing.JLabel();
+
+        setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
+
+        jComboBox1.setBackground(new java.awt.Color(204, 255, 204));
+        jComboBox1.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "Item 1", "Item 2", "Item 3", "Item 4" }));
+        add(jComboBox1, new org.netbeans.lib.awtextra.AbsoluteConstraints(290, 30, 170, -1));
+
+        jLabel2.setFont(new java.awt.Font("Yu Gothic", 1, 18)); // NOI18N
+        jLabel2.setForeground(new java.awt.Color(204, 255, 204));
+        jLabel2.setText("Consultar papa");
+        add(jLabel2, new org.netbeans.lib.awtextra.AbsoluteConstraints(40, 30, -1, -1));
+
+        jLabel3.setFont(new java.awt.Font("Sylfaen", 0, 18)); // NOI18N
+        jLabel3.setForeground(new java.awt.Color(255, 204, 255));
+        jLabel3.setText("Nombre");
+        add(jLabel3, new org.netbeans.lib.awtextra.AbsoluteConstraints(40, 80, -1, -1));
+
+        jLabel4.setFont(new java.awt.Font("Sylfaen", 0, 18)); // NOI18N
+        jLabel4.setForeground(new java.awt.Color(255, 204, 255));
+        jLabel4.setText("Especie");
+        add(jLabel4, new org.netbeans.lib.awtextra.AbsoluteConstraints(40, 120, -1, -1));
+
+        jLabel5.setFont(new java.awt.Font("Sylfaen", 0, 18)); // NOI18N
+        jLabel5.setForeground(new java.awt.Color(255, 204, 255));
+        jLabel5.setText("Zona de Producción");
+        add(jLabel5, new org.netbeans.lib.awtextra.AbsoluteConstraints(40, 160, -1, -1));
+
+        jLabel1.setFont(new java.awt.Font("Sylfaen", 0, 18)); // NOI18N
+        jLabel1.setForeground(new java.awt.Color(255, 204, 255));
+        jLabel1.setText("Hábito de crecimiento");
+        add(jLabel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(40, 210, -1, -1));
+
+        jLabel6.setFont(new java.awt.Font("Sylfaen", 0, 18)); // NOI18N
+        jLabel6.setForeground(new java.awt.Color(255, 204, 255));
+        jLabel6.setText("Floración");
+        add(jLabel6, new org.netbeans.lib.awtextra.AbsoluteConstraints(40, 260, -1, -1));
+
+        jLabel7.setFont(new java.awt.Font("Sylfaen", 0, 18)); // NOI18N
+        jLabel7.setForeground(new java.awt.Color(255, 204, 255));
+        jLabel7.setText("Bayas");
+        add(jLabel7, new org.netbeans.lib.awtextra.AbsoluteConstraints(40, 300, -1, -1));
+
+        jLabel8.setFont(new java.awt.Font("Sylfaen", 0, 18)); // NOI18N
+        jLabel8.setForeground(new java.awt.Color(255, 204, 255));
+        jLabel8.setText("Tubérculos");
+        add(jLabel8, new org.netbeans.lib.awtextra.AbsoluteConstraints(40, 340, -1, -1));
+
+        jTextField1.setBackground(new java.awt.Color(255, 204, 255));
+        jTextField1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jTextField1ActionPerformed(evt);
+            }
+        });
+        add(jTextField1, new org.netbeans.lib.awtextra.AbsoluteConstraints(230, 80, 200, -1));
+
+        jTextField2.setBackground(new java.awt.Color(255, 204, 255));
+        add(jTextField2, new org.netbeans.lib.awtextra.AbsoluteConstraints(230, 120, 200, -1));
+
+        jTextField3.setBackground(new java.awt.Color(255, 204, 255));
+        add(jTextField3, new org.netbeans.lib.awtextra.AbsoluteConstraints(230, 160, 200, -1));
+
+        jTextField4.setBackground(new java.awt.Color(255, 204, 255));
+        add(jTextField4, new org.netbeans.lib.awtextra.AbsoluteConstraints(230, 210, 200, -1));
+
+        jTextField5.setBackground(new java.awt.Color(255, 204, 255));
+        add(jTextField5, new org.netbeans.lib.awtextra.AbsoluteConstraints(230, 260, 200, -1));
+
+        jTextField6.setBackground(new java.awt.Color(255, 204, 255));
+        add(jTextField6, new org.netbeans.lib.awtextra.AbsoluteConstraints(230, 300, 200, -1));
+
+        jTextField7.setBackground(new java.awt.Color(255, 204, 255));
+        add(jTextField7, new org.netbeans.lib.awtextra.AbsoluteConstraints(230, 340, 200, -1));
+
+        jButton1.setText("LIMPIAR");
+        add(jButton1, new org.netbeans.lib.awtextra.AbsoluteConstraints(550, 230, -1, -1));
+
+        jButton2.setText("MODIFICAR");
+        add(jButton2, new org.netbeans.lib.awtextra.AbsoluteConstraints(540, 90, -1, -1));
+
+        jButton3.setText("ELIMINAR");
+        add(jButton3, new org.netbeans.lib.awtextra.AbsoluteConstraints(550, 160, -1, -1));
+
+        Fondo.setIcon(new javax.swing.ImageIcon(getClass().getResource("/view/PanelC.jpg"))); // NOI18N
+        add(Fondo, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, -1, -1));
     }// </editor-fold>//GEN-END:initComponents
+
+    private void jTextField1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jTextField1ActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_jTextField1ActionPerformed
 
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JLabel Fondo;
+    private javax.swing.JButton jButton1;
+    private javax.swing.JButton jButton2;
+    private javax.swing.JButton jButton3;
+    private javax.swing.JComboBox<String> jComboBox1;
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel2;
+    private javax.swing.JLabel jLabel3;
+    private javax.swing.JLabel jLabel4;
+    private javax.swing.JLabel jLabel5;
+    private javax.swing.JLabel jLabel6;
+    private javax.swing.JLabel jLabel7;
+    private javax.swing.JLabel jLabel8;
+    private javax.swing.JTextField jTextField1;
+    private javax.swing.JTextField jTextField2;
+    private javax.swing.JTextField jTextField3;
+    private javax.swing.JTextField jTextField4;
+    private javax.swing.JTextField jTextField5;
+    private javax.swing.JTextField jTextField6;
+    private javax.swing.JTextField jTextField7;
     // End of variables declaration//GEN-END:variables
 }
