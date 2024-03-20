c3d25be4a3ae9e362565972211d85b063486ab77
saritxm <saranicollemm@gmail.com>
2024-03-19 22:46:14 -0500

    yap

--- src/model/Conexion.java
+++ src/model/Conexion.java
@@ -13,7 +13,6 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class Conexion {
-<<<<<<< HEAD
 
     private static Connection cn = null; // Objeto para establecer la conexión con la base de datos
     private static String URLBD = "jdbc:mysql://localhost:3306/empresa"; // URL de la base de datos
@@ -23,29 +22,14 @@ public class Conexion {
     // Método estático para obtener una conexión a la base de datos
     public static Connection getConexion() {
         try {
-            // Intenta establecer la conexión con la base de datos utilizando los parámetros de conexión
-=======
-    // Toma la ruta de acceso a la base de datos
-    private static Connection cn = null;
-    private static String URLBD = "jdbc:mysql://localhost:3306/empresa";
-    private static String usuario = "root";
-    private static String contrasena = "";
-
-    public static Connection getConexion() {
-        try {
-            //obtiene la conexion con la base de datos usando los datos dados
->>>>>>> 1fadda0e4cf58b23b68bebc1d5427a8532ec4f6f
+            // Establecer la conexión con la base de datos utilizando los parámetros de conexión
             cn = DriverManager.getConnection(URLBD, usuario, contrasena);
         } catch (SQLException ex) {
             
         }
         return cn; // Devuelve la conexión establecida o null si ocurrió un error
     }
-<<<<<<< HEAD
-    // Método estático para desconectar la conexión a la base de datos
-=======
     //cierra la conexion
->>>>>>> 1fadda0e4cf58b23b68bebc1d5427a8532ec4f6f
     public static void desconectar() {
         cn = null; // Establece el objeto Connection como null para cerrar la conexión
     }
